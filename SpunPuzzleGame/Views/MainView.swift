3973ff9f230c3209403e01e381078b1e6332489d
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-23 09:46:54 +0900

    feat: add UIColor Hex extension and implement MainView with MVC structure

--- SpunPuzzleGame/Views/MainView.swift
+++ SpunPuzzleGame/Views/MainView.swift
@@ -1,8 +1,19 @@
-//
-//  MainView.swift
-//  SpunPuzzleGame
-//
-//  Created by 신호연 on 10/23/24.
-//
+import UIKit
 
-import Foundation
+class MainView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    // 뷰 설정 함수
+    private func setupView() {
+        self.backgroundColor = UIColor(hex: "#01B42F")
+    }
+}
