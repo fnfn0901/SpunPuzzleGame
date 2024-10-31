997bfac2e2b3192e2825708eeb1c95d22eb583be
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-31 16:49:11 +0900

    feat: PlayView에 퍼즐 묶음 및 정답존 UI 요소 추가
    - videoContainerView 아래에 345x175 크기의 퍼즐 묶음 뷰 추가 (회색 배경)
    - 퍼즐 묶음 뷰 하단 10pt 간격으로 228x228 크기의 정답존 뷰 추가 (E3E3E3 배경, 원형)
    - UI 요소 배치를 통해 퍼즐 및 정답존 레이아웃 구성 완료

--- SpunPuzzleGame/Views/PlayView.swift
+++ SpunPuzzleGame/Views/PlayView.swift
@@ -17,6 +17,21 @@ class PlayView: UIView {
     let videoContainerView = VideoContainerView()
     let navigationBar = CustomNavigationBar()
     
+    // 추가된 구성 요소
+    private let puzzleBundleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.gray // 퍼즐 묶음 회색으로 초기화
+        return view
+    }()
+    
+    private let answerZoneView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hex: "#E3E3E3") // 정답존 E3E3E3 색상
+        view.layer.cornerRadius = 114 // 원 모양
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
@@ -33,6 +48,8 @@ class PlayView: UIView {
         setupAlertView()
         setupProgressView()
         setupVideoContainer()
+        setupPuzzleBundleView()
+        setupAnswerZoneView()
     }
     
     private func setupNavigationBar() {
@@ -80,6 +97,25 @@ class PlayView: UIView {
             $0.top.equalTo(progressView.snp.bottom).offset(22)
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(videoContainerView.snp.width).multipliedBy(9.0 / 16.0)
+        }
+    }
+    
+    private func setupPuzzleBundleView() {
+        addSubview(puzzleBundleView)
+        puzzleBundleView.snp.makeConstraints {
+            $0.top.equalTo(videoContainerView.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(345)
+            $0.height.equalTo(175)
+        }
+    }
+    
+    private func setupAnswerZoneView() {
+        addSubview(answerZoneView)
+        answerZoneView.snp.makeConstraints {
+            $0.top.equalTo(puzzleBundleView.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(228) // 원형 영역
         }
     }
 }
